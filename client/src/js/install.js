430dfdcaf8c5e86a57e7ea1f5fe17d731b3d7d76
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-03-16 10:50:57 -0300

    initialize a click event handler on the  element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,12 +3,27 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // Adding an event handler to store triggered events
 window.addEventListener('beforeinstallprompt', (event) => {
+    
     window.deferredPropmt = event;
+
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// initialize a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    
+    const promptEvent = window.deferredPropmt;
+
+    if(!promptEvent) {
+        return;
+    }
+    //show prompt
+    promptEvent.prompt();
+    //reset the deferred propmt variable
+    window.deferredPropmt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
