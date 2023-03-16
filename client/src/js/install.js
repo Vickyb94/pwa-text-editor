6f8ac9b712f2cbebb16668bfe249efc4fd704742
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-03-16 10:46:58 -0300

    created a function to add an event handler to store triggered events

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Adding an event handler to store triggered events
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPropmt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
